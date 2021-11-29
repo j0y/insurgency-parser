1ce53f0720a833bff1a356935217bcb3676554c0
j0y <12984600+j0y@users.noreply.github.com>
2021-11-29 23:32:29 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	insurgencylog "my.com/insurgency-log"
+	"os"
+)
+
+// Usage:
+//
+// From file:
+// go run main.go example.log
+//
+// From STDIN:
+// cat example.log | go run main.go
+//
+// To File:
+// go run main.go > out.txt
+//
+// Omit errors:
+// go run main.go 2>/dev/null
+
+type matchInfoStruct struct {
+	Map      string `json:"map"`
+	Rounds   uint8  `json:"rounds"`
+	Duration uint32 `json:"duration"`
+	Won      bool   `json:"won"`
+}
+type playerStatsStruct struct {
+	Kills  uint32 `json:"kills"`
+	Deaths uint32 `json:"deaths"`
+}
+
+func main() {
+	var matchInfo matchInfoStruct
+	playerStats := make(map[string]playerStatsStruct)
+
+	var file *os.File
+	var err error
+
+	file, err = os.Open("example.log")
+	/*if len(os.Args) < 2 {
+		file = os.Stdin
+	} else {
+		file, err = os.Open(os.Args[1])
+	}*/
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	r := bufio.NewReader(file)
+
+	// read first line
+	l, _, err := r.ReadLine()
+
+	for err == nil {
+		message, errParse := insurgencylog.Parse(string(l))
+		if errParse != nil {
+			// print parse errors to stderr
+			fmt.Fprintf(os.Stderr, "ERROR: %s", insurgencylog.ToJSON(message))
+		} else {
+			// print to stdout
+			//fmt.Fprintf(os.Stdout, "%s", insurgencylog.ToJSON(m))
+		}
+
+		switch m := message.(type) {
+		case insurgencylog.LoadingMap:
+			matchInfo.Map = m.Map
+		case insurgencylog.PlayerKill:
+			if m.Attacker.SteamID == "BOT" && m.Victim.SteamID != "BOT" {
+				if stats, ok := playerStats[m.Victim.SteamID]; !ok {
+					playerStats[m.Victim.SteamID] = stats
+				} else {
+					stats.Kills++
+					playerStats[m.Victim.SteamID] = stats
+				}
+			}
+			if m.Victim.SteamID == "BOT" && m.Attacker.SteamID != "BOT" {
+				if stats, ok := playerStats[m.Attacker.SteamID]; !ok {
+					playerStats[m.Attacker.SteamID] = stats
+				} else {
+					stats.Kills++
+					playerStats[m.Attacker.SteamID] = stats
+				}
+			}
+		}
+
+		// next line
+		l, _, err = r.ReadLine()
+	}
+
+	fmt.Printf("%v", matchInfo)
+	fmt.Printf("%v", playerStats)
+}
