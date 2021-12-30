10d0487a479c6dc8c62009e5f88bb2c0107999fc
j0y <12984600+j0y@users.noreply.github.com>
2021-12-30 13:04:19 +0700

    add "die Hard" and "I Won"

--- medals/medals.go
+++ medals/medals.go
@@ -54,13 +54,155 @@ var medals = []int{
 func UpdateMedals() {
 	for _, medal := range medals {
 		switch medal {
-		case MedalObjectiveMostKillsCurrent:
+		/*case MedalObjectiveMostKillsCurrent:
 			err := checkMostKills()
+			if err != nil {
+				log.Fatal(err)
+			}
+		case MedalObjectiveHighestKDCurrent:
+
+		} */
+		case MedalObjectiveIWon:
+			err := checkIWon()
+			if err != nil {
+				log.Fatal(err)
+			}
+		case MedalObjectiveDieHard:
+			err := checkDieHard()
 			if err != nil {
 				log.Fatal(err)
 			}
 		}
 	}
+}
+
+func checkDieHard() error {
+	wonMatchesQuery := `
+select id, MAX(max_kills)
+from (
+         SELECT users.id, mus.kills as max_kills
+         from users
+                  LEFT JOIN match_user_stats mus on users.id = mus.user_id
+                  LEFT JOIN matches m on mus.match_id = m.id
+         WHERE m.won = true
+           AND mus.deaths = 0
+           AND mus.kills > 20
+         group by users.id, mus.kills) a
+GROUP BY id
+
+`
+	rows, err := dbp.DB.Query(wonMatchesQuery)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil
+		}
+		return err
+	}
+	defer rows.Close()
+
+	type playerStatsStruct struct {
+		ID       uint32 `json:"id"`
+		MaxKills uint32 `json:"max_kills"`
+	}
+
+	userStats := make([]playerStatsStruct, 0)
+	for rows.Next() {
+		var player playerStatsStruct
+		err = rows.Scan(&player.ID, &player.MaxKills)
+		if err != nil {
+			return err
+		}
+
+		userStats = append(userStats, player)
+	}
+
+	// get any error encountered during iteration
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+
+	for _, userStat := range userStats {
+		// checking if medal is already awarded
+		medalQuery := `SELECT value from user_medals where user_id = $1 AND medal_id = $2`
+
+		var medalKills uint32
+		err := dbp.DB.QueryRow(medalQuery, userStat.ID, MedalObjectiveDieHard).Scan(&medalKills)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				insertQuery := `INSERT INTO user_medals (user_id, medal_id, value) VALUES ($1, $2, $3)`
+				_, err = dbp.DB.Exec(insertQuery, userStat.ID, MedalObjectiveDieHard, userStat.MaxKills)
+				if err != nil {
+					return err
+				}
+			}
+			return err
+		} else {
+			if medalKills < userStat.MaxKills {
+				updateQuery := `UPDATE user_medals SET value = $1 WHERE user_id = $2 AND medal_id = $3`
+
+				_, err = dbp.DB.Exec(updateQuery, userStat.MaxKills, userStat.ID, MedalObjectiveDieHard)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func checkIWon() error {
+	wonMatchesQuery := `
+SELECT users.id, COUNT(*)
+from users
+         LEFT JOIN match_user_stats mus on users.id = mus.user_id
+         LEFT JOIN matches m on mus.match_id = m.id
+WHERE m.won = true
+group by users.id
+`
+	rows, err := dbp.DB.Query(wonMatchesQuery)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil
+		}
+		return err
+	}
+	defer rows.Close()
+
+	type playerStatsStruct struct {
+		ID    uint32 `json:"id"`
+		Count uint32 `json:"count"`
+	}
+
+	userStats := make([]playerStatsStruct, 0)
+	for rows.Next() {
+		var player playerStatsStruct
+		err = rows.Scan(&player.ID, &player.Count)
+		if err != nil {
+			return err
+		}
+
+		if player.Count >= 5 {
+			userStats = append(userStats, player)
+		}
+	}
+
+	// get any error encountered during iteration
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+
+	for _, userStat := range userStats {
+		insertQuery := `INSERT INTO user_medals (user_id, medal_id) VALUES ($1, $2)`
+		_, err = dbp.DB.Exec(insertQuery, userStat.ID, MedalObjectiveIWon)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func checkMostKills() error {
